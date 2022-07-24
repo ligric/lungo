9a5aa01c077c5004ebac549c2ef4bc7d281cfe5c
limeniye <limeniye@gmail.com>
2022-07-24 14:37:30 +0300

    - Added Latte.Wpf project;

--- src/Latte.Wpf.Shared/AttachedProperties/SolarEclipse.cs
+++ src/Latte.Wpf.Shared/AttachedProperties/SolarEclipse.cs
@@ -2,24 +2,21 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
-using System.Windows.Media;
 
+namespace Latte.Wpf;
 
-namespace Latte.Wpf.Shared.AttachedProperties
+public class SolarEclipse
 {
-    public class SolarEclipse
+    public static bool GetThemeChangingSubscribe(FrameworkElement element)
     {
-        public static bool GetThemeChangingSubscribe(FrameworkElement element)
-        {
-            return (bool)element.GetValue(ThemeChangingSubscribeProperty);
-        }
-
-        public static void SetThemeChangingSubscribe(FrameworkElement element, bool themeChangingSubscribe)
-        {
-            element.SetValue(ThemeChangingSubscribeProperty, themeChangingSubscribe);
-        }
-
-        public static readonly DependencyProperty ThemeChangingSubscribeProperty =
-            DependencyProperty.RegisterAttached("ThemeChangingSubscribe", typeof(bool), typeof(SolarEclipse), new PropertyMetadata(false));
+        return (bool)element.GetValue(ThemeChangingSubscribeProperty);
     }
+
+    public static void SetThemeChangingSubscribe(FrameworkElement element, bool themeChangingSubscribe)
+    {
+        element.SetValue(ThemeChangingSubscribeProperty, themeChangingSubscribe);
+    }
+
+    public static readonly DependencyProperty ThemeChangingSubscribeProperty =
+        DependencyProperty.RegisterAttached("ThemeChangingSubscribe", typeof(bool), typeof(SolarEclipse), new PropertyMetadata(false));
 }
