69ffa1ecd244e85d086548d72e7bb3bea254842e
limeniye <limeniye@gmail.com>
2022-07-24 13:26:58 +0300

    - Added SolarEclipse;

---
+++ src/Latte.Wpf.Shared/AttachedProperties/SolarEclipse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+
+namespace Latte.Wpf.Shared.AttachedProperties
+{
+    public class SolarEclipse
+    {
+        public static bool GetThemeChangingSubscribe(FrameworkElement element)
+        {
+            return (bool)element.GetValue(ThemeChangingSubscribeProperty);
+        }
+
+        public static void SetThemeChangingSubscribe(FrameworkElement element, bool themeChangingSubscribe)
+        {
+            element.SetValue(ThemeChangingSubscribeProperty, themeChangingSubscribe);
+        }
+
+        public static readonly DependencyProperty ThemeChangingSubscribeProperty =
+            DependencyProperty.RegisterAttached("ThemeChangingSubscribe", typeof(bool), typeof(SolarEclipse), new PropertyMetadata(false));
+    }
+}
