fac811db24267a8baa4d272eb99ed9c0351417a6
limeniye <limeniye@gmail.com>
2022-07-25 13:59:04 +0300

    - Window ranning;

---
+++ samples/Latte.Wpf.Sample.Shared/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Latte.Wpf.Sample.Windows;
+using System.Windows;
+
+namespace Latte.Wpf.Sample
+{
+    public partial class App
+    {
+        private void OnAppStartup(object sender, StartupEventArgs args)
+        {
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+        }
+    }
+}
---
+++ samples/Latte.Wpf.Sample.Shared/Windows/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Latte.Wpf.Sample.Windows
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
