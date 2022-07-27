16faac2f8cd0ecb9c547ce62bde9c0af26674364
limeniye <limeniye@gmail.com>
2022-07-27 14:38:37 +0300

    - Elements moved to Resources but animation doesn't work;

--- samples/Latte.Wpf.Sample.Shared/Windows/MainWindow.xaml.cs
+++ samples/Latte.Wpf.Sample.Shared/Windows/MainWindow.xaml.cs
@@ -36,37 +36,37 @@ namespace Latte.Wpf.Sample.Windows
 
 
 
-        private static Brush FrameworkElementToImmutableBrush(FrameworkElement source)
-        {
-            var currentRect = LayoutInformation.GetLayoutSlot(source);
+        //private static Brush FrameworkElementToImmutableBrush(FrameworkElement source)
+        //{
+        //    var currentRect = LayoutInformation.GetLayoutSlot(source);
 
-            int width = (int)Math.Round(source.ActualWidth + source.Margin.Left + source.Margin.Right, MidpointRounding.AwayFromZero);
-            int height = (int)Math.Round(source.ActualHeight + source.Margin.Top + source.Margin.Bottom, MidpointRounding.AwayFromZero);
+        //    int width = (int)Math.Round(source.ActualWidth + source.Margin.Left + source.Margin.Right, MidpointRounding.AwayFromZero);
+        //    int height = (int)Math.Round(source.ActualHeight + source.Margin.Top + source.Margin.Bottom, MidpointRounding.AwayFromZero);
 
 
-            Size size = new Size(width, height);
-            Rect rect = new Rect(size);
+        //    Size size = new Size(width, height);
+        //    Rect rect = new Rect(size);
 
-            source.Measure(size);
-            source.Arrange(rect);
-            source.UpdateLayout();
+        //    source.Measure(size);
+        //    source.Arrange(rect);
+        //    source.UpdateLayout();
 
-            RenderTargetBitmap rtb = new RenderTargetBitmap(width, height, 96, 96, PixelFormats.Default);
-            rtb.Render(source);
+        //    RenderTargetBitmap rtb = new RenderTargetBitmap(width, height, 96, 96, PixelFormats.Default);
+        //    rtb.Render(source);
 
-            DrawingGroup drawing = new DrawingGroup();
-            drawing.Children.Add(new ImageDrawing(rtb, rect));
-            drawing.ClipGeometry = new RectangleGeometry(new Rect(source.Margin.Left, source.Margin.Top, source.ActualWidth, source.ActualHeight));
+        //    DrawingGroup drawing = new DrawingGroup();
+        //    drawing.Children.Add(new ImageDrawing(rtb, rect));
+        //    drawing.ClipGeometry = new RectangleGeometry(new Rect(source.Margin.Left, source.Margin.Top, source.ActualWidth, source.ActualHeight));
 
-            ImageBrush brush = new ImageBrush(new DrawingImage(drawing));
-            brush.Freeze();
+        //    ImageBrush brush = new ImageBrush(new DrawingImage(drawing));
+        //    brush.Freeze();
 
-            source.Measure(new Size(currentRect.Width, currentRect.Height));
-            source.Arrange(currentRect);
-            source.UpdateLayout();
+        //    source.Measure(new Size(currentRect.Width, currentRect.Height));
+        //    source.Arrange(currentRect);
+        //    source.UpdateLayout();
 
-            return brush;
-        }
+        //    return brush;
+        //}
 
 
     }
