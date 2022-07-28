9aa06ec4c2dcf38a6e175a566c042a06ad2ed8a2
limeniye <limeniye@gmail.com>
2022-07-28 16:07:46 +0300

    - Animation throught AttachedProperty;

--- src/Latte.Wpf.Shared/AttachedProperties/SolarEclipse.cs
+++ src/Latte.Wpf.Shared/AttachedProperties/SolarEclipse.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 
 namespace Latte.Wpf;
@@ -73,6 +76,77 @@ public static class SolarEclipse
 
     private static void OnPropertyChanged(object sender, EventArgs e)
     {
+        FrameworkElement element = (FrameworkElement)sender;
+        element.BurntLeafDrowingBrush();
+    }
+}
 
+internal static class LatteBackgroudAnimationsHalper
+{
+    public static void BurntLeafDrowingBrush(this FrameworkElement element)
+    {
+        PathFigure topRightToLeftPoint = (PathFigure)element.FindResource("TopRightToLeftPoint");
+        LineSegment rightUpToDownPoint = (LineSegment)element.FindResource("RightUpToDownPoint");
+        LineSegment downRightToLeftPoint = (LineSegment)element.FindResource("DownRightToLeftPoint");
+        BezierSegment sezierSegment = (BezierSegment)element.FindResource("SezierSegment");
+        //SolidColorBrush backgroundBrush = (SolidColorBrush)element.FindResource("BackgroundBrush");
+        GeometryDrawing backgroundGeometryDrawing = (GeometryDrawing)element.FindResource("BackgroundGeometryDrawing");
+
+        element.MouseEnter += (s, e) =>
+        {
+            topRightToLeftPoint.BeginAnimation(PathFigure.StartPointProperty, new PointAnimation()
+            {
+                Duration = TimeSpan.FromSeconds(4),
+                From = new Point { X = 100, Y = 0 },
+                To = new Point { X = 0, Y = 0 }
+            });
+
+            rightUpToDownPoint.BeginAnimation(LineSegment.PointProperty, new PointAnimation()
+            {
+                Duration = TimeSpan.FromSeconds(2),
+                From = new Point { X = 100, Y = 0 },
+                To = new Point { X = 100, Y = 100 }
+            });
+
+            downRightToLeftPoint.BeginAnimation(LineSegment.PointProperty, new PointAnimation()
+            {
+                BeginTime = TimeSpan.FromSeconds(2),
+                Duration = TimeSpan.FromSeconds(4),
+                From = new Point { X = 100, Y = 100 },
+                To = new Point { X = 0, Y = 100 }
+            });
+
+            // ------------------------------------------------------------------------------------------
+
+            sezierSegment.BeginAnimation(BezierSegment.Point1Property, new PointAnimation()
+            {
+                BeginTime = TimeSpan.FromSeconds(2),
+                Duration = TimeSpan.FromSeconds(4),
+                From = new Point { X = 100, Y = 0 },
+                To = new Point { X = 0, Y = 100 }
+            });
+            sezierSegment.BeginAnimation(BezierSegment.Point2Property, new PointAnimation()
+            {
+                BeginTime = TimeSpan.FromSeconds(1),
+                Duration = TimeSpan.FromSeconds(2),
+                From = new Point { X = 100, Y = 0 },
+                To = new Point { X = 0, Y = 0 }
+            });
+            sezierSegment.BeginAnimation(BezierSegment.Point3Property, new PointAnimation()
+            {
+                Duration = TimeSpan.FromSeconds(1),
+                From = new Point { X = 100, Y = 0 },
+                To = new Point { X = 0, Y = 0 }
+            });
+
+            // ------------------------------------------------------------------------------------------
+            if (backgroundGeometryDrawing.Brush.IsFrozen)
+                backgroundGeometryDrawing.Brush = backgroundGeometryDrawing.Brush.CloneCurrentValue();
+
+            var colorAnimationUsingKeyFrames = new ColorAnimationUsingKeyFrames();
+            colorAnimationUsingKeyFrames.Duration = TimeSpan.FromSeconds(2);    
+            colorAnimationUsingKeyFrames.KeyFrames.Add(new EasingColorKeyFrame((Color)ColorConverter.ConvertFromString("#2b2731")));
+            backgroundGeometryDrawing.Brush.BeginAnimation(SolidColorBrush.ColorProperty, colorAnimationUsingKeyFrames);
+        };
     }
 }
