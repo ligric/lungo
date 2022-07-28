79ac1cff92d336ba8ba072c392bc98451d7ab406
limeniye <limeniye@gmail.com>
2022-07-28 14:05:48 +0300

    - Added ThemeChangingSubscribeProperty;

--- src/Latte.Wpf.Shared/AttachedProperties/SolarEclipse.cs
+++ src/Latte.Wpf.Shared/AttachedProperties/SolarEclipse.cs
@@ -1,9 +1,15 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
 
 namespace Latte.Wpf;
 
-public class SolarEclipse
+public static class SolarEclipse
 {
+    private static readonly List<TargetPropertyDescriptor> targets = new List<TargetPropertyDescriptor>();
+
     public static bool GetThemeChangingSubscribe(FrameworkElement element)
     {
         return (bool)element.GetValue(ThemeChangingSubscribeProperty);
@@ -15,5 +21,58 @@ public class SolarEclipse
     }
 
     public static readonly DependencyProperty ThemeChangingSubscribeProperty =
-        DependencyProperty.RegisterAttached("ThemeChangingSubscribe", typeof(bool), typeof(SolarEclipse), new PropertyMetadata(false));
+        DependencyProperty.RegisterAttached("ThemeChangingSubscribe", typeof(bool), 
+            typeof(SolarEclipse), new PropertyMetadata(false, OnThemeChangingSubscribeChanged));
+
+    private static void OnThemeChangingSubscribeChanged(DependencyObject @object, DependencyPropertyChangedEventArgs e)
+    {
+        bool isNewValueSubscribe = (bool)e.NewValue;
+        FrameworkElement element = (FrameworkElement)@object;
+
+        int index = targets.FindIndex(0, targets.Count, x => x.Source == element);
+
+        if (!isNewValueSubscribe && index >= 0)
+        {
+            targets[index].PropertyChanged -= OnPropertyChanged;
+            targets.RemoveAt(index);
+        }
+        else if(isNewValueSubscribe && index == -1)
+        {
+            BackgroundSubscribe(element);
+        }
+        else
+        {
+            throw new NotImplementedException($"isNewValueSubscribe is {isNewValueSubscribe} but index is {index}.");
+        }
+    }
+
+    private static void BackgroundSubscribe(FrameworkElement element)
+    {
+        TargetPropertyDescriptor target = null;
+
+        if (element is Panel elementPanel)
+        {
+            target = new TargetPropertyDescriptor(Guid.NewGuid().ToString(), elementPanel, nameof(elementPanel.Background));
+        }
+        else if (element is Border elementBorder)
+        {
+            target = new TargetPropertyDescriptor(Guid.NewGuid().ToString(), elementBorder, nameof(elementBorder.Background));
+        }
+        else if (element is Shape elementShape)
+        {
+            target = new TargetPropertyDescriptor(Guid.NewGuid().ToString(), elementShape, nameof(elementShape.Fill));
+        }
+        else
+        {
+            throw new NotImplementedException($"The element {element} is not Panel or Border or Shape.");
+        }
+
+        target.PropertyChanged += OnPropertyChanged;
+        targets.Add(target);
+    }
+
+    private static void OnPropertyChanged(object sender, EventArgs e)
+    {
+
+    }
 }
---
+++ src/Latte.Wpf.Shared/TargetPropertyDescriptor.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Latte.Wpf
+{
+
+    /// <summary>
+    /// Documentation: https://www.cyberforum.ru/wpf-silverlight/thread2650880.html#post14575691
+    /// </summary>
+    internal class TargetPropertyDescriptor
+    {
+        /// <summary>Instance name.</summary>
+        public string Name { get; }
+
+        /// <summary>Source of date.</summary>
+        /// <remarks>obj</remarks>
+        public object Source { get; }
+
+        /// <summary>Tracked property name.</summary>
+        public string PropertyName { get; }
+
+        /// <summary>Descriptor for property.</summary>
+        public PropertyDescriptor PropertyDescriptor { get; }
+
+        private object _value;
+
+        public object Value
+        {
+            get => _value;
+            set
+            {
+                if ((_value == null && value == null) || (_value != null && _value.Equals(value))) 
+                    return;
+
+                if (value.GetType() == PropertyDescriptor.PropertyType)
+                {
+                    PropertyDescriptor.SetValue(Source, _value = value);
+                }
+                else
+                {
+                    try
+                    {
+                        PropertyDescriptor.SetValue(Source, _value = PropertyDescriptor.Converter.ConvertFrom(value));
+                    }
+                    catch
+                    {
+                        _value = value;
+                    }
+                }
+
+                System.Diagnostics.Debug.WriteLine($"{Name}.Value={value}");
+            }
+        }
+
+        public event EventHandler<EventArgs> PropertyChanged;
+
+        /// <param name="name">Instance name.</param>
+        public TargetPropertyDescriptor(string name, object source, string propertyName)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentNullException(nameof(name));
+
+            Name = name;
+            Source = source ?? throw new ArgumentNullException(nameof(source));
+            PropertyName = propertyName;
+
+            PropertyDescriptor = TypeDescriptor.GetProperties(Source).Find(PropertyName, false)
+                ?? throw new ArgumentException("The property not found.", nameof(PropertyName));
+
+            PropertyDescriptor.AddValueChanged(Source, PropertyValueChanged);
+        }
+
+        private void PropertyValueChanged(object sender, EventArgs e)
+        {
+            Value = PropertyDescriptor.GetValue(Source);
+
+            PropertyChanged?.Invoke(sender, e);
+        }
+    }
+}
