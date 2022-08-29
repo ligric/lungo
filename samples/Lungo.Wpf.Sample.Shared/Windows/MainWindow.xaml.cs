153980e064ec980b1d224d6738aea99361974d9f
limeniye <limeniye@gmail.com>
2022-08-29 23:16:46 +0300

    - New visual sample;

--- samples/Lungo.Wpf.Sample.Shared/Windows/MainWindow.xaml.cs
+++ samples/Lungo.Wpf.Sample.Shared/Windows/MainWindow.xaml.cs
@@ -1,20 +1,25 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace Lungo.Wpf.Sample.Windows
 {
     public sealed partial class MainWindow : Window
     {
+        private bool isDarkTheme = false;
+
         public MainWindow()
         {
             this.InitializeComponent();
+            themeChangerButton.Click += OnTestButtonClicked;
         }
 
-        private bool isDarkTheme = false;
         private void OnTestButtonClicked(object sender, RoutedEventArgs e)
         {
-            GeneralTransform generalTransform = button.TransformToVisual((Visual)button.Parent);
-            Rect rect = generalTransform.TransformBounds(new Rect(button.RenderSize));
+            GeneralTransform generalTransform = themeChangerButton.TransformToVisual((Visual)themeChangerButton.Parent);
+            Rect rect = generalTransform.TransformBounds(new Rect(themeChangerButton.RenderSize));
 
             if (isDarkTheme)
             {
@@ -27,41 +32,5 @@ namespace Lungo.Wpf.Sample.Windows
 
             isDarkTheme = !isDarkTheme;
         }
-
-
-
-        //private static Brush FrameworkElementToImmutableBrush(FrameworkElement source)
-        //{
-        //    var currentRect = LayoutInformation.GetLayoutSlot(source);
-
-        //    int width = (int)Math.Round(source.ActualWidth + source.Margin.Left + source.Margin.Right, MidpointRounding.AwayFromZero);
-        //    int height = (int)Math.Round(source.ActualHeight + source.Margin.Top + source.Margin.Bottom, MidpointRounding.AwayFromZero);
-
-
-        //    Size size = new Size(width, height);
-        //    Rect rect = new Rect(size);
-
-        //    source.Measure(size);
-        //    source.Arrange(rect);
-        //    source.UpdateLayout();
-
-        //    RenderTargetBitmap rtb = new RenderTargetBitmap(width, height, 96, 96, PixelFormats.Default);
-        //    rtb.Render(source);
-
-        //    DrawingGroup drawing = new DrawingGroup();
-        //    drawing.Children.Add(new ImageDrawing(rtb, rect));
-        //    drawing.ClipGeometry = new RectangleGeometry(new Rect(source.Margin.Left, source.Margin.Top, source.ActualWidth, source.ActualHeight));
-
-        //    ImageBrush brush = new ImageBrush(new DrawingImage(drawing));
-        //    brush.Freeze();
-
-        //    source.Measure(new Size(currentRect.Width, currentRect.Height));
-        //    source.Arrange(currentRect);
-        //    source.UpdateLayout();
-
-        //    return brush;
-        //}
-
-
     }
 }
