3bd9de78b7d9ae7caa3bd954900253fdc3c98558
limeniye <limeniye@gmail.com>
2022-09-03 15:06:13 +0300

    - New view;

--- samples/Lungo.Wpf.Sample.Shared/Windows/MainWindow.xaml.cs
+++ samples/Lungo.Wpf.Sample.Shared/Windows/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace Lungo.Wpf.Sample.Windows
 {
@@ -15,18 +17,53 @@ namespace Lungo.Wpf.Sample.Windows
             this.InitializeComponent();
         }
 
-        private void OnThemeChangerButtonClicked(object sender, RoutedEventArgs e)
+        private static Brush FrameworkElementToImmutableBrush(FrameworkElement source)
         {
-            if (isDarkTheme)
-            {
-                SolarEclipseService.ChangeTheme(themeChangerButton, (Color)FindResource("Light"));
-            }
-            else
-            {
-                SolarEclipseService.ChangeTheme(themeChangerButton, (Color)FindResource("Dark"));
-            }
+            var currentRect = LayoutInformation.GetLayoutSlot(source);
 
-            isDarkTheme = !isDarkTheme;
+            int width = (int)Math.Round(source.ActualWidth + source.Margin.Left + source.Margin.Right, MidpointRounding.AwayFromZero);
+            int height = (int)Math.Round(source.ActualHeight + source.Margin.Top + source.Margin.Bottom, MidpointRounding.AwayFromZero);
+
+
+            Size size = new Size(width, height);
+            Rect rect = new Rect(size);
+
+            source.Measure(size);
+            source.Arrange(rect);
+            source.UpdateLayout();
+
+            RenderTargetBitmap rtb = new RenderTargetBitmap(width, height, 96, 96, PixelFormats.Default);
+            rtb.Render(source);
+
+            DrawingGroup drawing = new DrawingGroup();
+            drawing.Children.Add(new ImageDrawing(rtb, rect));
+            drawing.ClipGeometry = new RectangleGeometry(new Rect(source.Margin.Left, source.Margin.Top, source.ActualWidth, source.ActualHeight));
+
+            ImageBrush brush = new ImageBrush(new DrawingImage(drawing));
+            brush.Freeze();
+
+            source.Measure(new Size(currentRect.Width, currentRect.Height));
+            source.Arrange(currentRect);
+            source.UpdateLayout();
+
+            return brush;
         }
+
+
+        //private void OnThemeChangerButtonClicked(object sender, RoutedEventArgs e)
+        //{
+        //    if (isDarkTheme)
+        //    {
+        //        SolarEclipseService.ChangeTheme(themeChangerButton, (Color)FindResource("Light"));
+        //    }
+        //    else
+        //    {
+        //        SolarEclipseService.ChangeTheme(themeChangerButton, (Color)FindResource("Dark"));
+        //    }
+
+        //    isDarkTheme = !isDarkTheme;
+        //}
+
+
     }
 }
