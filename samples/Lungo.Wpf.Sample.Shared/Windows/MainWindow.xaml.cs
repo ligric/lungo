6ec7e6697b05b1ce0b0ea9d026ca6a06458d2d06
limeniye <limeniye@gmail.com>
2022-09-05 03:10:50 +0300

    - Ellipse from ArcSegments;

--- samples/Lungo.Wpf.Sample.Shared/Windows/MainWindow.xaml.cs
+++ samples/Lungo.Wpf.Sample.Shared/Windows/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
@@ -21,43 +22,38 @@ namespace Lungo.Wpf.Sample.Windows
 
         private void OnTestButtonClicked(object sender, RoutedEventArgs e)
         {
-            Brush brush = FrameworkElementToImmutableBrush(ellipse);
 
-            rectangle0.Fill = brush;
-            rectangle1.Fill = brush;
-            rectangle2.Fill = brush;
         }
 
-        private static Brush FrameworkElementToImmutableBrush(FrameworkElement source)
+        public static IReadOnlyList<Point> GetPolygonVertices(int n, double r, Vector center)
         {
-            var currentRect = LayoutInformation.GetLayoutSlot(source);
+            Point[] points = new Point[n];
+            double segmentAngle = 2 * Math.PI / n; // Угол сегмента в радианах.
+            for (int i = 0; i < n; i++)
+            {
+                double angle = segmentAngle * i; // Угол очередной вершины.
+                Point vertic = new Point(r * Math.Cos(angle), r * Math.Sin(angle)); // Точка вершины без смещения центра.
+                vertic += center; // Добавляем смещение центра.
+                points[i] = vertic; // Запоминание точки вершины.
+            }
+            return Array.AsReadOnly(points); // Возврат вершин в массиве только для чтения.
+        }
 
-            int width = (int)Math.Round(source.ActualWidth + source.Margin.Left + source.Margin.Right, MidpointRounding.AwayFromZero);
-            int height = (int)Math.Round(source.ActualHeight + source.Margin.Top + source.Margin.Bottom, MidpointRounding.AwayFromZero);
+        private Geometry GetCircleGeometry()
+        {
+            PathSegmentCollection pathSegments = new PathSegmentCollection();
+            var points = GetPolygonVertices(8, 200, new Vector(200, 200));
 
+            foreach (var point in points)
+                pathSegments.Add(new ArcSegment(point, new Size(180, 180), 0, false, SweepDirection.Clockwise, true));
 
-            Size size = new Size(width, height);
-            Rect rect = new Rect(size);
+            pathSegments.Add(new ArcSegment(points[0], new Size(180, 180), 0, false, SweepDirection.Clockwise, true));
 
-            source.Measure(size);
-            source.Arrange(rect);
-            source.UpdateLayout();
+            PathFigure pathFigure = new PathFigure(points[0], pathSegments, true);
+            PathFigureCollection pathFigures = new PathFigureCollection();
+            pathFigures.Add(pathFigure);
 
-            RenderTargetBitmap rtb = new RenderTargetBitmap(width, height, 96, 96, PixelFormats.Default);
-            rtb.Render(source);
-
-            DrawingGroup drawing = new DrawingGroup();
-            drawing.Children.Add(new ImageDrawing(rtb, rect));
-            drawing.ClipGeometry = new RectangleGeometry(new Rect(source.Margin.Left, source.Margin.Top, source.ActualWidth, source.ActualHeight));
-
-            ImageBrush brush = new ImageBrush(new DrawingImage(drawing));
-            brush.Freeze();
-
-            source.Measure(new Size(currentRect.Width, currentRect.Height));
-            source.Arrange(currentRect);
-            source.UpdateLayout();
-
-            return brush;
+            return new PathGeometry(pathFigures);
         }
     }
 }
