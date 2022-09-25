e7f63ba44439d17e290bbf3ee2202263ef32f38f
limeniye <limeniye@gmail.com>
2022-09-25 09:23:45 +0300

    - Added WinUI project;

---
+++ samples/Lungo.WinUI.Sample/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace Lungo.WinUI.Sample
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
